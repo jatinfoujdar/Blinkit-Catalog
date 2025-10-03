508ec40243095997654cc96348a2866d37dda512
Jatin Foujdar <jatin321001@gmail.com>
2025-10-03 21:53:56 +0530

    splash screen commit

---
+++ Blinkit-Catalog/SplashViewController.swift
@@ -0,0 +1,78 @@
+//
+//  SplashViewController.swift
+//  Blinkit-Catalog
+//
+//  Created by jatin foujdar on 03/10/25.
+//
+
+import UIKit
+import SwiftUI
+
+class SplashViewController: UIViewController {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Blinkit"
+        label.font = .systemFont(ofSize: 28, weight: .bold)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "By - Jatin Foujdar"
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        view.backgroundColor = UIColor(red: 1.0, green: 0.85, blue: 0.4, alpha: 1.0)
+        
+        view.addSubview(titleLabel)
+        view.addSubview(subtitleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -20),
+            
+            subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 12),
+            subtitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+}
+
+// MARK: - SwiftUI Preview
+
+struct SplashViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewControllerPreview {
+            SplashViewController()
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+/// Helper to preview UIViewControllers in SwiftUI
+struct ViewControllerPreview<ViewController: UIViewController>: UIViewControllerRepresentable {
+    let viewControllerBuilder: () -> ViewController
+
+    init(_ builder: @escaping () -> ViewController) {
+        self.viewControllerBuilder = builder
+    }
+
+    func makeUIViewController(context: Context) -> ViewController {
+        viewControllerBuilder()
+    }
+
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {}
+}
+
